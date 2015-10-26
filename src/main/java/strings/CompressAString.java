0c6fca574091d5f8e343c8ef04919949fb99266f
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2015-10-26 13:56:24 -0400

    few more tasks

---
+++ src/main/java/strings/CompressAString.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package strings;
+
+/**
+ * @author burlutal
+ *
+ *         Implement a method to perform basic string compression using the
+ *         counts of repeated characters. For example, the string aabcccccaaa
+ *         would become a2blc5a3. If the "compressed" string would not become
+ *         smaller than the original string, your method should return the
+ *         original string.
+ * 
+ */
+public class CompressAString {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(compress("aabcccccaaa"));
+	}
+
+	public static void processStringWithStrategy(String string, FoundDoublicateStrategy strategy) {
+		char curr = string.charAt(0);
+		int count = 1;
+		for (int i = 1; i < string.length(); i++) {
+			if (curr != string.charAt(i)) {
+				strategy.performOperation(curr, count);
+				curr = string.charAt(i);
+				count = 1;
+			} else {
+				count++;
+			}
+		}
+		if (count > 0) {
+			strategy.performOperation(curr, count);
+		}
+	}
+
+	public static String compress(String string) {
+		GetNewLengthStrategy strategy = new GetNewLengthStrategy();
+		processStringWithStrategy(string, strategy);
+		int newlength = strategy.getNewlength();
+		if (newlength < string.length()) {
+			CompressStrategy cstrategy = new CompressStrategy(newlength);
+			processStringWithStrategy(string, cstrategy);
+			return cstrategy.getCompressedString();
+		}
+		return string;
+	}
+
+	private static interface FoundDoublicateStrategy {
+		void performOperation(char curr, int count);
+	}
+
+	private static class GetNewLengthStrategy implements FoundDoublicateStrategy {
+		private int newlength = 0;
+
+		@Override
+		public void performOperation(char curr, int count) {
+			newlength++;
+			newlength += lettersInNumber(count);
+		}
+
+		public int getNewlength() {
+			return newlength;
+		}
+
+		private static int lettersInNumber(int count) {
+			int result = 1;
+			while ((count /= 10) > 0)
+				result++;
+			return result;
+		}
+	}
+
+	private static class CompressStrategy implements FoundDoublicateStrategy {
+		private final StringBuffer sb;
+
+		public CompressStrategy(int newlenght) {
+			super();
+			this.sb = new StringBuffer(newlenght);
+		}
+
+		@Override
+		public void performOperation(char curr, int count) {
+			add(sb, curr, count);
+		}
+
+		private static void add(StringBuffer sb, char curr, int count) {
+			sb.append(curr);
+			sb.append(count);
+		}
+
+		public String getCompressedString() {
+			return sb.toString();
+		}
+	}
+
+}
---
+++ src/main/java/strings/ShiftWordsInString.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package strings;
+
+/**
+ * @author burlutal Write a method to replace all spaces in a string with'%20'.
+ *         You may assume that the string has sufficient space at the end of the
+ *         string to hold the additional characters, and that you are given the
+ *         "true" length of the string. (Note: if implementing in Java, please
+ *         use a character array so that you can perform this operation in
+ *         place.) EXAMPLE Input: "Mr John Smith Output: "Mr%20Dohn%20Smith"
+ *
+ */
+public class ShiftWordsInString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String
+		 str = "I am Mr John Smith";
+//		 str = " I am Mr John Smith ";
+//		 str = " ";
+//		str = "  ";
+		char[] input = new char[100];
+		System.arraycopy(str.toCharArray(), 0, input, 0, str.length());
+		replace2(input, str.length());
+		for (char c : input) {
+			System.out.print(c);
+		}
+	}
+
+	public static void replace2(char[] string, int originalLength) {
+		char[] webspace = new char[] { '%', '2', '0' };
+		boolean lookingWhereWordStarts = false;
+		// 1. go through and check how many spaces
+		int spaces = 0;
+		for (int t = string.length - 1; t >= 0; t--) {
+			if (string[t] == ' ')
+				spaces++;
+		}
+		// 2. shift
+		lookingWhereWordStarts = false;
+		int wordEndIndex = -1;
+		for (int t = originalLength - 1; t >= 0; t--) {
+			if (string[t] != ' ') {
+				if (!lookingWhereWordStarts) {
+					lookingWhereWordStarts = true;
+					wordEndIndex = t;
+				}
+			} else {
+				lookingWhereWordStarts = false;
+				if (wordEndIndex != -1)
+					System.arraycopy(string, t + 1, string, t + spaces * 2 + 1, wordEndIndex - t);
+				System.arraycopy(webspace, 0, string, t + spaces * 2 - 2, 3);
+				spaces--;
+			}
+		}
+	}
+
+}
