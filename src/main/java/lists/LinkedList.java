7581ecfefd5057d271dfc9310ba692bc2b573405
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2015-10-27 16:46:05 -0400

    recursive find kh to last

--- src/main/java/lists/LinkedList.java
+++ src/main/java/lists/LinkedList.java
@@ -1,7 +1,10 @@
 package lists;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 
@@ -10,6 +13,7 @@ import java.util.Set;
 public class LinkedList<T> implements List<T> {
 	Node<T> root;
 	Node<T> tail;
+	int size = 0;
 
 	class Node<T> {
 		Node<T> next;
@@ -25,10 +29,12 @@ public class LinkedList<T> implements List<T> {
 			tail.next = node;
 		}
 		tail = node;
+		size++;
 	}
 
 	/**
-	 * Assuming we can use HashSet, if not we can sort if first and then remove duplicates
+	 * Assuming we can use HashSet, if not we can sort if first and then remove
+	 * duplicates
 	 * 
 	 * @param list
 	 */
@@ -50,14 +56,63 @@ public class LinkedList<T> implements List<T> {
 		} while (node != null);
 	}
 
+	public T nthToLast(int k) {
+		if (k <= 0)
+			return null;
+		Node<T> p1 = root;
+		Node<T> p2 = root;
+		for (int i = 0; i < k - 1; i++) {
+			if (p2 == null)
+				return null; // Error check
+			p2 = p2.next;
+		}
+		if (p2 == null)
+			return null;
+
+		while (p2.next != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1.value;
+	}
+
+	public T nthToLastRecurcive(int k) {
+		AtomicReference<T> result = new AtomicReference<>();
+		AtomicInteger counter = new AtomicInteger();
+		findRecursevely(root, k, result, counter);
+		return result.get();
+	}
+
+	private void findRecursevely(Node<T> node, int k, AtomicReference<T> result, AtomicInteger counter) {
+		if(node.next != null)
+			findRecursevely(node.next, k, result, counter);
+		if(result.get() == null){
+			if(counter.get() == k){
+				result.set(node.value);
+			} else {
+				counter.incrementAndGet();
+			}
+		}
+	}
+
 	public void print() {
 		Node<T> node = root;
 		do {
 			System.out.print(node.value);
 			node = node.next;
-			if(node != null)
+			if (node != null)
 				System.out.print(",");
 		} while (node != null);
 		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(5);
+		list.add(1);
 	}
 }
