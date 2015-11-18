b4a89a2e447223c28082a20e8cb55d65b0b0afcf
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2015-11-18 17:26:19 -0500

    continue bst implementation

--- src/main/java/trees/BST.java
+++ src/main/java/trees/BST.java
@@ -1,81 +1,173 @@
 package trees;
 
 /**
+ *
+ * Recursive implementation of binary search tree
+ * 
  * @author burlutal
  *
  */
-public class BST<T extends Comparable<T>> {
+public class BST<K extends Comparable<K>, V> {
 	TreeNode root;
 	long size;
 
 	class TreeNode {
 		TreeNode left, right;
-		T value;
-		public TreeNode(TreeNode left, TreeNode right, T value) {
+		K key;
+		V value;
+
+		public TreeNode(TreeNode left, TreeNode right, K key, V value) {
 			super();
 			this.left = left;
 			this.right = right;
+			this.key = key;
 			this.value = value;
 		}
+
 		@Override
 		public String toString() {
-			return "[" + value + "," + left + "," + right + "]";
+			return "[" + key + ":" + value + "," + left + "," + right + "]";
 		}
 	}
 
-	public boolean contains(T value) {
-		return contains(root, value);
+	public V get(K key) {
+		return get(root, key);
 	}
 
-	private boolean contains(TreeNode node, T value){
-		if(node == null)
-			return false;
-		int compareTo = value.compareTo(node.value);
-		if(compareTo < 0) {
-			return contains(node.left, value);
+	private V get(TreeNode node, K key) {
+		if (node == null)
+			return null;
+		int compareTo = key.compareTo(node.key);
+		if (compareTo < 0) {
+			return get(node.left, key);
 		} else if (compareTo > 0) {
-			return contains(node.right, value);
+			return get(node.right, key);
+		} else
+			return node.value;
+	}
+
+	public boolean contains(K key) {
+		return contains(root, key);
+	}
+
+	private boolean contains(TreeNode node, K key) {
+		if (node == null)
+			return false;
+		int compareTo = key.compareTo(node.key);
+		if (compareTo < 0) {
+			return contains(node.left, key);
+		} else if (compareTo > 0) {
+			return contains(node.right, key);
 		} else
 			return true;
 	}
-	
-	public void put(T value) {
-		put(root, value);
+
+	public void put(K key, V value) {
+		root = put(root, key, new TreeNode(null, null, key, value));
 	}
 
-	private TreeNode put(TreeNode node, T value){
-		if(node == null){
-			return new TreeNode(null, null, value);
+	private TreeNode put(TreeNode node, K key, TreeNode newNode) {
+		if (node == null) {
+			return newNode;
 		} else {
-			int compareTo = value.compareTo(node.value);
-			if(compareTo <= 0){
-				node.left = put(node.left, value);
+			int compareTo = key.compareTo(node.key);
+			if (compareTo <= 0) {
+				node.left = put(node.left, key, newNode);
 			} else {
-				node.right = put(node.right, value);
+				node.right = put(node.right, key, newNode);
 			}
 		}
 		return node;
 	}
-	
-	public T min() {
-		TreeNode node = root;
-		while(node.left != null){
-			node = node.left;
-		}
-		return node.value;
+
+	public V min() {
+		if (root == null)
+			return null;
+		return min(root);
 	}
 
-	public T max() {
-		TreeNode node = root;
-		while(node.right != null){
-			node = node.right;
+	private V min(TreeNode node) {
+		if (node.left == null)
+			return node.value;
+		return min(node.left);
+	}
+
+	public V max() {
+		if (root == null)
+			return null;
+		return max(root);
+	}
+
+	private V max(TreeNode node) {
+		if (node.right == null)
+			return node.value;
+		else
+			return max(node.right);
+	}
+
+	public void deleteMax() {
+		if (root == null)
+			return;
+		root = deleteMax(root);
+	}
+
+	private TreeNode deleteMax(TreeNode node) {
+		if (node.right == null) {
+			return null;
+		} else {
+			node.right = deleteMax(node.right);
 		}
-		return node.value;
+		return node;
+	}
+
+	public void deleteMin() {
+		if (root == null)
+			return;
+		root = deleteMin(root);
+	}
+
+	private TreeNode deleteMin(TreeNode node) {
+		if (node.left == null) {
+			return null;
+		} else {
+			node.left = deleteMin(node.left);
+		}
+		return node;
+	}
+
+	public V remove(K key) {
+		if (root == null)
+			return null;
+		return remove(null, root, key);
+	}
+
+	private V remove(TreeNode parent, TreeNode node, K key) {
+		int compareTo = key.compareTo(node.key);
+		if (compareTo < 0) {
+			return remove(node, node.left, key);
+		} else if (compareTo > 0) {
+			return remove(node, node.right, key);
+		} else {
+			if (parent == null) {
+				root = null;
+			} else {
+				if (parent == node.left) {
+					parent.left = null;
+				} else {
+					parent.right = null;
+				}
+			}
+			if (node.left != null)
+				root = put(root, node.left.key, node.left);
+			if (node.right != null)
+				root = put(root, node.right.key, node.right);
+			return node.value;
+		}
 	}
 
 	@Override
 	public String toString() {
-		return "BST [root=" + root + "]";
+		return root != null ? root.toString() : "";
 	}
-	
+
 }
--- src/main/java/trees/BSTTest.java
+++ src/main/java/trees/BSTTest.java
@@ -1,6 +1,7 @@
 package trees;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,23 +9,105 @@ import org.junit.Test;
  *
  */
 public class BSTTest {
+	BST<Integer, Integer> tree = new BST<>();
 
-	@Test public void test(){
-		BST<Integer> tree = new BST<>();
-		tree.put(4);
-		tree.put(1);
-		tree.put(8);
-		tree.put(9);
-		tree.put(6);
-		tree.put(12);
+	@Before
+	public void setup() {
+		tree.put(4, 4);
+		tree.put(1, 1);
+		tree.put(8, 8);
+		tree.put(9, 9);
+		tree.put(6, 6);
+		tree.put(12, 12);
+	}
+
+	@Test
+	public void test() {
 		Assert.assertEquals(1, tree.min().intValue());
 		Assert.assertEquals(12, tree.max().intValue());
+		Assert.assertEquals(9, tree.get(9).intValue());
+		Assert.assertEquals(1, tree.get(1).intValue());
+		Assert.assertEquals(12, tree.get(12).intValue());
 		Assert.assertTrue(tree.contains(12));
 		Assert.assertTrue(tree.contains(1));
 		Assert.assertTrue(tree.contains(9));
 		Assert.assertFalse(tree.contains(5));
 		Assert.assertFalse(tree.contains(7));
 		Assert.assertFalse(tree.contains(11));
-		
 	}
+
+	@Test
+	public void testEmpty() {
+		tree = new BST<>();
+		Assert.assertNull(tree.min());
+		Assert.assertNull(tree.max());
+		Assert.assertFalse(tree.contains(1));
+		Assert.assertNull(tree.remove(1));
+	}
+
+	@Test
+	public void testOneElement() {
+		tree = new BST<>();
+		tree.put(1, 1);
+		Assert.assertEquals(1, tree.min().intValue());
+		Assert.assertEquals(1, tree.max().intValue());
+		Assert.assertTrue(tree.contains(1));
+		Assert.assertFalse(tree.contains(2));
+	}
+
+	@Test
+	public void testDeleteMax() {
+		tree.deleteMax();
+		Assert.assertEquals(9, tree.max().intValue());
+	}
+
+	@Test
+	public void testDeleteMin() {
+		tree.deleteMin();
+		Assert.assertEquals(4, tree.min().intValue());
+	}
+
+	@Test
+	public void testRemoveWhole() {
+		tree.remove(8);
+		Assert.assertFalse(tree.contains(8));
+		Assert.assertTrue(tree.contains(6));
+		Assert.assertTrue(tree.contains(9));
+		Assert.assertTrue(tree.contains(12));
+		Assert.assertEquals(12, tree.max().intValue());
+	}
+
+	@Test
+	public void testRemoveRight() {
+		tree.remove(9);
+		Assert.assertFalse(tree.contains(9));
+		Assert.assertTrue(tree.contains(6));
+		Assert.assertTrue(tree.contains(8));
+		Assert.assertTrue(tree.contains(12));
+		Assert.assertEquals(12, tree.max().intValue());
+	}
+
+	@Test
+	public void testRemoveLeft() {
+		tree.remove(9);
+		Assert.assertFalse(tree.contains(9));
+		Assert.assertTrue(tree.contains(6));
+		Assert.assertTrue(tree.contains(8));
+		Assert.assertTrue(tree.contains(12));
+		Assert.assertEquals(12, tree.max().intValue());
+	}
+
+	@Test
+	public void testRemoveRoot() {
+		tree.remove(4);
+		Assert.assertFalse(tree.contains(4));
+		Assert.assertTrue(tree.contains(1));
+		Assert.assertTrue(tree.contains(6));
+		Assert.assertTrue(tree.contains(8));
+		Assert.assertTrue(tree.contains(12));
+		Assert.assertEquals(12, tree.max().intValue());
+		Assert.assertEquals(1, tree.min().intValue());
+	}
+	
+
 }
