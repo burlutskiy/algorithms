fcd4f6b07fc0356fea474abdcf046552a6b77050
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2015-12-21 15:05:28 -0500

    PreOrder and mind map update

---
+++ src/main/java/graphs/PreOrder.java
@@ -0,0 +1,30 @@
+package graphs;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrder<V> {
+	private List<V> order;
+
+	public PreOrder(AbstractDigraph<V> graph) {
+		final LinkedList<V> list = new LinkedList<>();
+		GraphVisitor<V> visitor = new GraphVisitor<V>() {
+			@Override
+			public void visitNode(V object) {
+				list.add(object);
+			}
+		};
+		new Order<>(graph, visitor, GraphVisitor.Direction.PRE);
+		if (!list.isEmpty()) {
+			order = list;
+		}
+	}
+
+	public boolean hasOrder() {
+		return order != null;
+	}
+
+	public Iterable<V> order() {
+		return order;
+	}
+}
