95acd5c13541aafd4ba023a9d61306b9fea84b9e
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2016-01-19 18:30:16 -0500

    scrum planning

---
+++ knowledge/Product Backlog Refinement.mm
@@ -0,0 +1,168 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453229460077" ID="ID_741737997" MODIFIED="1453245094224" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      Scrum
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1453245029233" ID="ID_867214077" MODIFIED="1453245037766" POSITION="left" TEXT="Iteration Planning Meeting"/>
+<node CREATED="1453245043801" ID="ID_1705348098" MODIFIED="1453245065054" POSITION="right" TEXT="Product Backlog Refinement">
+<node CREATED="1453229476735" ID="ID_704320553" MODIFIED="1453235308910" TEXT="The purpose of the meeting">
+<node CREATED="1453235087456" ID="ID_1150221984" MODIFIED="1453235196089">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ensure that the Product Backlog<br />is ready for the next sprint.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1453235132557" ID="ID_1898693085" MODIFIED="1453235151194" TEXT="Make all stories clear for every one."/>
+<node CREATED="1453235152380" HGAP="22" ID="ID_1053121872" MODIFIED="1453237563542" TEXT="Estimate the stories." VSHIFT="8"/>
+</node>
+</node>
+<node CREATED="1453229503653" FOLDED="true" HGAP="22" ID="ID_407984116" MODIFIED="1453245019528" TEXT="Story Points" VSHIFT="15">
+<node CREATED="1453229507733" ID="ID_1244528910" MODIFIED="1453235682544" TEXT="Story Points are about effort, not complexity."/>
+<node CREATED="1453235683699" HGAP="27" ID="ID_831931904" MODIFIED="1453238219744" TEXT="Story Points not the hours. " VSHIFT="-2">
+<node CREATED="1453235987522" ID="ID_405642629" MODIFIED="1453236001375" TEXT="You should not say one story point = eight hours."/>
+</node>
+<node CREATED="1453236012897" HGAP="30" ID="ID_1755286229" MODIFIED="1453238218128" TEXT="Story Points are not about complexity."/>
+<node CREATED="1453235853177" HGAP="28" ID="ID_1744536576" MODIFIED="1453238216048" TEXT="Story Point are relative measure." VSHIFT="-3">
+<node CREATED="1453238851736" ID="ID_1581469856" MODIFIED="1453238852341" TEXT="Story points are a relative measure of the size of a user story. A user story estimated as ten story points is twice as big, complex, or risky as a story estimated as five story points. A ten-point story is similarly half as big, complex, or risky as a twenty-point story. What matters are the relative values assigned to different stories. "/>
+</node>
+<node CREATED="1453237276228" HGAP="18" ID="ID_153416367" MODIFIED="1453237554631" TEXT="Why" VSHIFT="19">
+<node CREATED="1453237293411" ID="ID_1063032267" MODIFIED="1453237293975" TEXT="Estimation is a waste. Estimation of tasks in hours is utter waste, people spend hours debating minutia, when they would better off just starting. Even estimation of stories in points is a waste, however to make your projects progress predictable and transparent this requires that stories be roughly the same size +/- one Delta. For most teams (mature or not) this turns out to be difficult, so they stick to story points."/>
+<node CREATED="1453237295851" HGAP="26" ID="ID_1509352965" MODIFIED="1453237551926" TEXT="Story points are therefore faster, better, and cheaper than hours and the highest performing teams completely abandon any hourly estimation as they view it as waste that just slows them down." VSHIFT="17"/>
+<node CREATED="1453237436915" HGAP="23" ID="ID_1187413800" MODIFIED="1453237548758" TEXT="Perhaps the way to say that is that points are a function of effort, risk and uncertainty, or SP = f(E, R, U). (Call one of those complexity if you want; it&#x2019;s not important.) The idea is that points are an estimate of the effort involved. Risk, uncertainty, complexity, doubt and other things people have mentioned here can be incorporated BUT only to the extent they affect the expected effort. If something is complex but that complexity will not affect the time to implement the feature, that complexity should not affect the estimate&#x2014;that was my point with the lists of numbers to be multiplied or added." VSHIFT="15"/>
+</node>
+</node>
+<node CREATED="1453229805581" FOLDED="true" HGAP="27" ID="ID_799781651" MODIFIED="1453245594647" TEXT="Estimations" VSHIFT="6">
+<node CREATED="1453236638988" ID="ID_1830310896" MODIFIED="1453238542707" TEXT="Velocity">
+<node CREATED="1453238552279" HGAP="25" ID="ID_850298080" MODIFIED="1453238577344" TEXT="Velocity is a measure of a team&#x2019;s rate of progress." VSHIFT="9">
+<node CREATED="1453238573678" ID="ID_1547159829" MODIFIED="1453238574106" TEXT="It is calculated by summing the number of story points assigned to each user story that the team completed during the iteration."/>
+</node>
+<node CREATED="1453238801336" ID="ID_1475345917" MODIFIED="1453238802412" TEXT="Velocity Corrects Estimation Errors">
+<node CREATED="1453242371895" ID="ID_1874595890" MODIFIED="1453242397313" TEXT="We&#x2019;re mostly interested in the team&#x2019;s average velocity over time">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1453242446447" HGAP="24" ID="ID_1009141816" MODIFIED="1453242449017" TEXT="Because the estimate for each feature is made relative to the estimates for other features, it does not matter if our estimates are correct, a little incorrect, or a lot incorrect. What matters is that they are consistent. " VSHIFT="6"/>
+</node>
+<node CREATED="1453239150829" HGAP="25" ID="ID_1866064787" MODIFIED="1453242399672" TEXT="Accuracy of estimation looks like a curve" VSHIFT="25"/>
+<node CREATED="1453239237549" ID="ID_78136318" MODIFIED="1453239245257" TEXT="The Estimation Scale">
+<node CREATED="1453239302414" ID="ID_1473295516" MODIFIED="1453239314428" TEXT="1,2,3,5 and 8">
+<node CREATED="1453239389313" ID="ID_955967886" MODIFIED="1453239441469" TEXT="Non-Linear Fibonacci sequance works better cause they&#xa; reflect the greater uncertainty associated with estimates for larger units of work.">
+<node CREATED="1453239603999" ID="ID_951130190" MODIFIED="1453239604684" TEXT="Each of these numbers should be thought of as a bucket into which items of the appropriate size are poured. Rather than thinking of work as water being poured into the buckets, think of the work as sand. If you are estimating using 1, 2, 3, 5, and 8 and have a story that you think is just the slightest bit bigger than the other five-point stories you&#x2019;ve estimated, it would be OK to put it into the five-point bucket. A story you think is a 7, however, clearly would not fit in the five-point bucket"/>
+</node>
+</node>
+<node CREATED="1453239874934" ID="ID_965030280" MODIFIED="1453239876411" TEXT="Epics">
+<node CREATED="1453239963327" ID="ID_984090807" MODIFIED="1453239975043" TEXT="13, 20, 40 and 100"/>
+</node>
+<node CREATED="1453239899476" ID="ID_1506778184" MODIFIED="1453239900824" TEXT="Themes"/>
+</node>
+<node CREATED="1453240000507" ID="ID_1128059149" MODIFIED="1453240008775" TEXT="Deriving An Estimate">
+<node CREATED="1453240036105" HGAP="27" ID="ID_501438899" MODIFIED="1453240176240" TEXT="Analogy" VSHIFT="5">
+<node CREATED="1453236040606" ID="ID_1750073305" MODIFIED="1453236610930">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pick one average and assign lets say 3sp.<br />Then measure the other in comparison with the one you picked.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1453236292336" ID="ID_1804983905" MODIFIED="1453236621009" TEXT="Two developers can start by estimating a given user story as one&#xa; point even if their individual estimates of the actual time on task&#xa; differ. Starting with that estimate, they can then agree to estimate&#xa; something as two points if each agree it will take twice as long &#xa;as the first story."/>
+<node CREATED="1453238045872" HGAP="21" ID="ID_556967932" MODIFIED="1453238210393" TEXT="Example: Suppose you and I look at a building a certain distance away. You think it&#x2019;s a 5-minute walk. I think it&#x2019;s a 10-minute walk because I&#x2019;m on crutches at the time we have this discussion. You and I cannot agree. You are right that it is 5 minutes (for you) and I am right that it is 10 minutes (for me). In minutes, hours, days, etc. the problem is intractable: you and I cannot agree on an estimate because we produce at different rates. &#x2028; We can, however, agree that the building is &#x201c;1 unit of time away.&#x201d; You&#x2019;re thinking 5 minutes and I&#x2019;m thinking 10 minutes when we agree to that estimate of 1 unit&#x2014;but that doesn&#x2019;t matter. We&#x2019;ve found an estimate we can agree on." VSHIFT="3"/>
+</node>
+<node CREATED="1453240186333" HGAP="21" ID="ID_710783996" MODIFIED="1453240439403" TEXT="An alternative to expert opinion comes in the form of estimating by analogy, which is what we&#x2019;re doing when we say &#x201c;this story is a little bigger than that story.&#x201d; When estimating by analogy, the estimator compares the story being esti- Disaggregation | 55 mated to one or more other stories. If the story is twice the size, it is given an estimate twice as large. There is evidence that we are better at estimating relative size than we are at estimating absolute size (Lederer 1998; Vicinanza 1991). When estimating this way you do not compare all stories against a single baseline or universal reference. Instead, you want to estimate each new story against an assortment of those that have already been estimated. This is referred to as triangulation. To triangulate, compare the story being estimated against a couple of other stories. To decide if a story should be estimated at five story points, see if it seems a little bigger than a story you estimated at three and a little smaller than a story you estimated at eight. " VSHIFT="9"/>
+</node>
+<node CREATED="1453240218668" HGAP="37" ID="ID_570197379" MODIFIED="1453240378658" TEXT="Disaggregation" VSHIFT="7">
+<node CREATED="1453240233542" HGAP="26" ID="ID_363539234" MODIFIED="1453240279054" TEXT="Disaggregation refers to splitting a story or feature into smaller, easier-to-estimate pieces. " VSHIFT="2"/>
+<node CREATED="1453242925219" HGAP="21" ID="ID_854347407" MODIFIED="1453242941646" TEXT="Spliting up the story (epic)" VSHIFT="22">
+<node CREATED="1453243016722" ID="ID_1407262173" MODIFIED="1453243017352" TEXT="Split large stories along the boundaries of the data supported by the story."/>
+<node CREATED="1453243137838" ID="ID_190177714" MODIFIED="1453243156606">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Split large stories based on the operations that are performed within the story.<br />
+    </p>
+    A common approach to doing this is to split a story along the boundaries of the common CRUD operations&#8212;Create, Read, Update, and Delete.
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1453243319010" ID="ID_172650654" MODIFIED="1453243320334" TEXT="Consider removing cross-cutting concerns (such as security, logging, error handling, and so on) and creating two versions of the story: one with and one without support for the cross-cutting concern."/>
+<node CREATED="1453243353261" ID="ID_1491326889" MODIFIED="1453243353721" TEXT="Considering splitting a large story by separating the functional and nonfunctional aspects into separate stories."/>
+<node CREATED="1453243406615" ID="ID_9094546" MODIFIED="1453243407243" TEXT="Separate a large story into smaller stories if the priorities of the smaller stories are different."/>
+<node CREATED="1453243453888" ID="ID_1128643977" MODIFIED="1453243486271">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Don&#8217;t split a large story into tasks. Like<br />
+    </p>
+    Code the user interface<br />Write the middle tier
+  </body>
+</html></richcontent>
+<node CREATED="1453243521914" ID="ID_143668221" MODIFIED="1453243522295" TEXT="The best way to avoid this temptation is to follow Hunt and Thomas&#x2019; advice (1999) to fire a tracer bullet through the system. A tracer bullet travels through all layers of a feature. That may mean delivering a partial user interface, a partial middle tier, and a partial database. Delivering a cohesive subset of all layers of a feature is almost always better than delivering all of one layer."/>
+</node>
+</node>
+</node>
+<node CREATED="1453240364831" HGAP="30" ID="ID_1986425597" MODIFIED="1453240377473" TEXT="Planning Pocker" VSHIFT="8">
+<node CREATED="1453240546974" ID="ID_190557336" MODIFIED="1453240547338" TEXT="Planning poker combines expert opinion, analogy, and disaggregation into an enjoyable approach to estimating that results in quick but reliable estimates. "/>
+<node CREATED="1453240997995" ID="ID_1236887969" MODIFIED="1453241021669" TEXT="Attendees: team or sub-set of team."/>
+<node CREATED="1453241201120" ID="ID_658551112" MODIFIED="1453241204405" TEXT="When">
+<node CREATED="1453241205009" ID="ID_1413389795" MODIFIED="1453241220799" TEXT="Two times"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1453241259217" ID="ID_1117682407" MODIFIED="1453241270572" TEXT="Re-Estimation">
+<node CREATED="1453241536366" ID="ID_477677667" MODIFIED="1453241546048" TEXT="When to re-estimate">
+<node CREATED="1453241523237" ID="ID_1919458391" MODIFIED="1453241638318">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Re-Estimating When Relative Size Changes.e.i R
+    </p>
+    e-estimates each of the charting stories.
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1453241647097" ID="ID_1601733857" MODIFIED="1453241783901" TEXT="Re-Estimation of Partially done story">
+<node CREATED="1453241784720" ID="ID_891777106" MODIFIED="1453241878656" TEXT="First, let me point out that I&#x2019;m generally in favor of an all-or-nothing stance&#xa;toward counting velocity: if a story is done (coded, tested, and accepted by the&#xa;product owner) then the team earns all the points, but if anything on the story&#xa;isn&#x2019;t done, they earn nothing. At the end of an iteration, this is the easiest case to&#xa;assess: If everything is done they get all the points; if anything is missing they&#xa;get no points. If the team is likely to take on the remaining portion of the story&#xa;in the next iteration this works well. Their velocity in the first iteration is a bit&#xa;lower than expected because they got no credit for partially completing a story.&#xa;In the second iteration, however, their velocity will be higher than expected because&#xa;they&#x2019;ll get all of the points even though some work had been completed&#xa;prior to the start of the iteration. This works well as long as everyone remembers&#xa;that we&#x2019;re mostly interested in the team&#x2019;s average velocity over time, not in&#xa;whether velocity jumped up or down in a given iteration"/>
+</node>
+<node CREATED="1453241964121" ID="ID_1501415382" MODIFIED="1453241967821" TEXT="However, the two best solutions to allocating points for incomplete stories are first not to have any incomplete stories and second to use sufficiently small stories that partial credit isn&#x2019;t an issue.">
+<node CREATED="1453242210048" ID="ID_321420405" MODIFIED="1453242210686" TEXT="Use re-estimating as a learning experience for estimating future user stories. As Tom Poppendieck has taught me, &#x201c;Failure to learn is the only true failure.&#x201d; Learn from each re-estimated story and turn the experience into a success."/>
+</node>
+</node>
+<node CREATED="1453229845467" FOLDED="true" ID="ID_701869241" MODIFIED="1453245022363" TEXT="Definition of Ready" VSHIFT="14">
+<node CREATED="1453229906397" ID="ID_631259098" MODIFIED="1453234384632" TEXT="Bug">
+<node CREATED="1453229911981" ID="ID_1142852948" MODIFIED="1453234384632" TEXT="Actual Result"/>
+<node CREATED="1453229932589" ID="ID_749126703" MODIFIED="1453234384632" TEXT="Expected Result"/>
+</node>
+</node>
+<node CREATED="1453230459567" HGAP="19" ID="ID_706169236" MODIFIED="1453236935691" TEXT="Definition Of Done" VSHIFT="6"/>
+<node CREATED="1453229790516" ID="ID_1419878720" MODIFIED="1453236934634" TEXT="Acceptance Critera" VSHIFT="15"/>
+</node>
+</node>
+</map>
