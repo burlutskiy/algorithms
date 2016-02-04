28a3090cd1dc37791f748e4d1479697e64154007
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2016-02-04 17:15:29 -0500

    refactor mm, deck and randomized queue impl

---
+++ knowledge/Trees.mm
@@ -0,0 +1,162 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#990000" CREATED="1366207444205" HGAP="114" ID="ID_576007995" LINK="algorithms.mm" MODIFIED="1454514325894" STYLE="bubble" TEXT="Trees" VSHIFT="14">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1382966761040" ID="ID_1787598862" MODIFIED="1454514342494" POSITION="left" TEXT="Quad Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1447962643469" ID="ID_334347111" MODIFIED="1454514325894" POSITION="left" STYLE="bubble" TEXT="Binary Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#338800" CREATED="1366207499279" HGAP="21" ID="ID_1952880802" MODIFIED="1454514387531" TEXT="Rotation" VSHIFT="2">
+<node COLOR="#338800" CREATED="1447962674877" MODIFIED="1454514325894" TEXT="Left"/>
+<node COLOR="#338800" CREATED="1447962677381" MODIFIED="1454514325894" TEXT="Right"/>
+</node>
+<node COLOR="#338800" CREATED="1447962890167" HGAP="38" MODIFIED="1454514325894" TEXT="Mirror / Reverse" VSHIFT="10"/>
+<node COLOR="#338800" CREATED="1387296405693" HGAP="37" ID="ID_471031098" MODIFIED="1454514377685" TEXT="Lookup" VSHIFT="3">
+<node COLOR="#999999" CREATED="1387296423520" MODIFIED="1454514325894" TEXT="This is not a divide and conquer algorithm because, although there are two recursive calls, only one is used at each level of the recursion">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1447962779329" HGAP="35" ID="ID_1264210711" MODIFIED="1454514379715" TEXT="Size" VSHIFT="4"/>
+<node COLOR="#338800" CREATED="1447962781322" HGAP="31" ID="ID_1266591018" MODIFIED="1454514380667" TEXT="Max Depth" VSHIFT="3"/>
+<node COLOR="#338800" CREATED="1449002620702" HGAP="33" ID="ID_1907658402" MODIFIED="1454514381531" TEXT="Print Tree" VSHIFT="-1"/>
+<node COLOR="#338800" CREATED="1447962819133" HGAP="40" ID="ID_224835981" MODIFIED="1454514383331" TEXT="Root To Leaf Path" VSHIFT="-3"/>
+<node COLOR="#338800" CREATED="1366207489718" HGAP="41" ID="ID_936205814" MODIFIED="1454514385555" TEXT="Traversal" VSHIFT="-1">
+<node COLOR="#338800" CREATED="1447952570095" MODIFIED="1454514325896" TEXT="Depth-First">
+<node COLOR="#338800" CREATED="1447952600660" MODIFIED="1454514325897" TEXT="Pre-Order"/>
+<node COLOR="#338800" CREATED="1447952596576" MODIFIED="1454514325897" TEXT="In-Order"/>
+<node COLOR="#338800" CREATED="1447952590304" MODIFIED="1454514325897" TEXT="Post-order"/>
+</node>
+</node>
+<node CREATED="1447962893223" HGAP="39" ID="ID_1247494738" MODIFIED="1454514325897" TEXT="Double a tree" VSHIFT="-51"/>
+</node>
+<node COLOR="#ff0000" CREATED="1382966750712" HGAP="26" ID="ID_1185546908" MODIFIED="1454514325897" POSITION="right" TEXT="K-Dimentinal Tree" VSHIFT="3">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1366207459547" HGAP="22" ID="ID_21637313" MODIFIED="1454514348789" POSITION="right" TEXT="Binary Search Tree" VSHIFT="127">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#338800" CREATED="1366207465844" MODIFIED="1454514325898" TEXT="Insert"/>
+<node CREATED="1366207476687" MODIFIED="1454514325898" TEXT="Search">
+<node COLOR="#ff0000" CREATED="1447952705601" MODIFIED="1454514325898" TEXT="Monte Carlo Tree Search MCTS"/>
+<node COLOR="#338800" CREATED="1366207586962" MODIFIED="1454514325898" TEXT="Breadth-first search (BFS)"/>
+<node COLOR="#338800" CREATED="1366207602242" MODIFIED="1454514325898" TEXT="Depth-first search (DFS)"/>
+</node>
+<node COLOR="#338800" CREATED="1366207480499" MODIFIED="1454514325898" TEXT="Delete">
+<node CREATED="1447962948896" MODIFIED="1454514325899" TEXT="T. Hibbard">
+<node CREATED="1447962950457" HGAP="27" MODIFIED="1454514325899" TEXT="successor" VSHIFT="6"/>
+<node CREATED="1447962953297" MODIFIED="1454514325899" TEXT="predecessor" VSHIFT="9"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1447952668858" HGAP="136" MODIFIED="1454514325899" TEXT="Delete Max" VSHIFT="42"/>
+<node COLOR="#338800" CREATED="1447952673466" HGAP="136" ID="ID_407899951" MODIFIED="1454514355590" TEXT="Delete Min" VSHIFT="40"/>
+<node COLOR="#ff0000" CREATED="1449238272338" HGAP="134" ID="ID_943578624" MODIFIED="1454514357357" TEXT="The Great Tree-List Recursion Problem" VSHIFT="-1">
+<node COLOR="#999999" CREATED="1449238505460" MODIFIED="1454514325900" TEXT="http://cslibrary.stanford.edu/109/TreeListRecursion.html">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node CREATED="1447962966781" HGAP="135" ID="ID_1317538881" MODIFIED="1454514358213" TEXT="Rank" VSHIFT="-2"/>
+<node CREATED="1449760990115" HGAP="128" ID="ID_1979202670" MODIFIED="1454514359645" TEXT="Range Select" VSHIFT="-5"/>
+<node CREATED="1449761000635" HGAP="119" ID="ID_505019423" MODIFIED="1454514360916" TEXT="Select" VSHIFT="-5"/>
+<node CREATED="1447962969505" HGAP="130" ID="ID_717904079" MODIFIED="1454514361917" TEXT="Floor" VSHIFT="-2"/>
+<node CREATED="1447962973898" HGAP="144" MODIFIED="1454514325900" TEXT="Ceiling" VSHIFT="9"/>
+<node CREATED="1366207510685" HGAP="57" ID="ID_706019558" MODIFIED="1454514369156" TEXT="Balanced Binary Search Trees" VSHIFT="-5">
+<node CREATED="1366207522044" FOLDED="true" ID="ID_82038873" MODIFIED="1454514370261" TEXT="Red-Black Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1449240504883" MODIFIED="1454514325901">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <table class="infobox" style="font-size: 12.32px; border-spacing: 3px; color: black; margin-top: 0; margin-right: 0px; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0; clear: right; line-height: 1.5em; font-family: sans-serif; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(249, 249, 249)">
+      <tr>
+        <td style="vertical-align: top; text-align: left">
+          
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font size="2">Average </font>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font size="2">Worst case </font>
+        </td>
+      </tr>
+      <tr>
+        <th style="vertical-align: top; text-align: left">
+          <font size="2">Space </font>
+        </th>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(<i>n</i>)</font>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(<i>n</i>)</font>
+        </td>
+      </tr>
+      <tr>
+        <th style="vertical-align: top; text-align: left">
+          <font size="2">Search </font>
+        </th>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+      </tr>
+      <tr>
+        <th style="vertical-align: top; text-align: left">
+          <font size="2">Insert </font>
+        </th>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+      </tr>
+      <tr>
+        <th style="vertical-align: top; text-align: left">
+          <font size="2">Delete </font>
+        </th>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+          <font face="Nimbus Roman No9 L, Times New Roman, Times, serif" size="2">O(log&#160;<i>n</i>)</font>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1449240704479" MODIFIED="1454514325909" TEXT="Left-Leaning Red&#x2013;Black Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1449240722038" MODIFIED="1454514325909" TEXT="Robert Sedgewick 2008"/>
+<node CREATED="1449253951853" MODIFIED="1454514325909" TEXT="https://www.cs.princeton.edu/~rs/talks/LLRB/RedBlack.pdf"/>
+<node CREATED="1449638274130" MODIFIED="1454514325910" TEXT="https://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf"/>
+</node>
+</node>
+<node CREATED="1447965971779" MODIFIED="1454514325910" TEXT="B-Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1449501759802" MODIFIED="1454514325910" TEXT="2-3-4-Tree">
+<node COLOR="#999999" CREATED="1449501787520" HGAP="29" MODIFIED="1454514325910" TEXT="2-3-4 trees are B-trees of order 4" VSHIFT="9">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1448997224737" MODIFIED="1454514325910" TEXT="Splay Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1448997234954" MODIFIED="1454514325910" TEXT="AVL Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1449501807157" MODIFIED="1454514325910" TEXT="Georgy Adelson Velskii and Yevgeniy Landis"/>
+</node>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1449240274823" HGAP="29" ID="ID_583768813" MODIFIED="1454514346484" POSITION="right" TEXT="Fusion Tree" VSHIFT="-126">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1382966756962" ID="ID_351293037" MODIFIED="1454514325897" POSITION="left" TEXT="Interval Tree">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
