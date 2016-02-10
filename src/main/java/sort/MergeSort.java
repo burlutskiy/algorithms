31f4142735a84d02f1bf1d94519e936850ee8990
Alexey Burlutskiy <burlutskiy.a.s@gmail.com>
2016-02-10 15:35:54 -0500

    trick with switching roles a and aux saves time and cutoff for short ar

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -14,36 +14,36 @@ public class MergeSort extends AbstractSort {
 
 	void sort() {
 		int[] aux = new int[a.length];
-		sort(a, aux, 0, a.length - 1);
+		System.arraycopy(a, 0, aux, 0, a.length);
+		sort(aux, a, 0, a.length - 1);
 	}
 	
 	private void sort(int[] a, int[] aux, int lo, int hi){
 		if(hi <= lo + CUTOFF - 1){
-			InsertionSort.sort(a, lo, hi);
+			InsertionSort.sort(aux, lo, hi);
 			return;
 		}
+		if(hi <= lo)
+			return;
 		int mid = lo + (hi - lo) / 2;
-		sort(a, aux, lo, mid);
-		sort(a, aux, mid + 1, hi);
+		sort(aux, a, lo, mid);
+		sort(aux, a, mid + 1, hi);
 		merge(a, aux, lo, mid, hi);
 	}
 	
 	public void merge(int[] a, int[] aux, int lo, int mid, int hi){
-		int n1 = mid - lo + 1; 
-		int n2 = hi - mid;
-		System.arraycopy(a, lo, aux, lo, n1 + n2);
 		int i = lo, j = mid + 1, k = lo;
 		while (i <= mid && j <= hi) {
-			if(less(i, j, aux))
-				a[k] = aux[i++];
+			if(less(i, j, a))
+				aux[k] = a[i++];
 			else
-				a[k] = aux[j++];
+				aux[k] = a[j++];
 			k = increase(k);
 		}
 		if(i <= mid)
-			System.arraycopy(aux, i, a, k, mid - i + 1);
+			System.arraycopy(a, i, aux, k, mid - i + 1);
 		if(j <= hi)
-			System.arraycopy(aux, j, a, k, hi - j + 1);
+			System.arraycopy(a, j, aux, k, hi - j + 1);
 	}
 
 }
