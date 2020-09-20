aa1686db606634bfbc4f7848137be57ec2600771
burlutskiy <burlutskiy.a.s@gmail.com>
2020-09-20 18:35:37 -0400

    moved from algorithms

--- knowledge/CryptoFinanceBiz.mm
+++
@@ -1,4 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1589161644048" ID="ID_277286645" MODIFIED="1589161653250" TEXT="CryptoFinanceBiz"/>
-</map>
