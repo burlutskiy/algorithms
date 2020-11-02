fe1a7034bfe039b3083c103f16083bf8e4cc0f0d
burlutskiy <burlutskiy.a.s@gmail.com>
2020-11-02 10:09:03 -0500

    combinations with given sum improvement

--- src/main/java/recursion/CombinationsWithGivenSum.java
+++ src/main/java/recursion/CombinationsWithGivenSum.java
@@ -2,11 +2,18 @@ package recursion;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -23,37 +30,54 @@ import org.junit.Test;
  */
 public class CombinationsWithGivenSum {
 
-	void findCombinations(int k, int n, Consumer<List<Integer>> consumer) {
-		int[] arr = new int[k];
-		for (int i = 0; i < 10; i++) {
-			arr[0] = i + 1;
-			find(arr, 0, k, n, consumer);
-		}
+	void findCombinations(int k, int n, Consumer<Collection<Integer>> consumer) {
+		find(new Stack<Integer>(), k, n, consumer);
 	}
 
-	void find(int[] arr, int index, int k, int n, Consumer<List<Integer>> consumer) {
-		int sum = IntStream.range(0, index  + 1).map(i -> arr[i]).sum();
-		if(sum > n)
+	void find(Stack<Integer> stack, int k, int n, Consumer<Collection<Integer>> consumer) {
+		int sum = stack.stream().mapToInt(i -> i).sum();
+		if (sum > n)
 			return;
-		if (index == k - 1) {
+		if (stack.size() == k) {
 			if (sum == n)
-				consumer.accept(Arrays.stream(arr).boxed().collect(Collectors.toList()));
+				consumer.accept(stack);
 			return;
 		} else {
-			for (int i = arr[index] + 1; i < 10; i++) {
-				arr[index + 1] = i;
-				find(arr, index + 1, k, n, consumer);
+			int i = 1;
+			if (!stack.isEmpty())
+				i = stack.peek() + 1;
+			for (; i < 10; i++) {
+				stack.push(i);
+				find(stack, k, n, consumer);
+				stack.pop();
 			}
 		}
 	}
 
 	@Test
 	public void test1() {
-		findCombinations(3, 15, triplet -> System.out.println(triplet));
+		Assert.assertArrayEquals(new int[][] { { 1, 5, 9 }, { 1, 6, 8 }, { 2, 4, 9 }, { 2, 5, 8 }, { 2, 6, 7 }, { 3, 4, 8 }, { 3, 5, 7 }, { 4, 5, 6 } },
+				find(3, 15));
 	}
-	
+
 	@Test
 	public void test2() {
-		findCombinations(4, 20, triplet -> System.out.println(triplet));
+		Assert.assertArrayEquals(new int[][] { { 1, 2, 8, 9 }, { 1, 3, 7, 9 }, { 1, 4, 6, 9 }, { 1, 4, 7, 8 }, { 1, 5, 6, 8 }, { 2, 3, 6, 9 }, { 2, 3, 7, 8 },
+				{ 2, 4, 5, 9 }, { 2, 4, 6, 8 }, { 2, 5, 6, 7 }, { 3, 4, 5, 8 }, { 3, 4, 6, 7 } }, find(4, 20));
+	}
+
+	int[][] find(int k, int n) {
+		final Set<List<Integer>> set = new LinkedHashSet<>();
+		Consumer<Collection<Integer>> consumer = collection -> {
+			List<Integer> list = new ArrayList<>(collection);
+			list.sort(Comparator.naturalOrder());
+			set.add(list);
+		};
+		findCombinations(k, n, consumer);
+		int[][] result = new int[set.size()][k];
+		int i = 0;
+		for (List<Integer> list : set)
+			result[i++] = list.stream().mapToInt(j -> j).toArray();
+		return result;
 	}
 }
